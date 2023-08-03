23cb2916c0aa3f22ff6f598e85ad288428d2f462
YuriiBonislavskiy <130743571+YuriiBonislavskiy@users.noreply.github.com>
2023-08-03 15:00:06 +0300

    HW 10

--- src/get-refs.js
+++ src/get-refs.js
@@ -1,6 +1,6 @@
 export default function getRefs() {
   return {
-    catSearch: document.querySelector('.breed-select'),
+    breedSelect: document.querySelector('.breed-select'),
     loadingText: document.querySelector('.loader'),
     errorText: document.querySelector('.error'),
     selectOption: document.querySelector('.breed-select selected'),
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import './css/common.css';
 import getRefs from './get-refs';
+const refs = getRefs();
+import SlimSelect from 'slim-select';
+import './css/common.css';
+import './css/slimselect.css';
 import * as API from './cat-api.js';
 
-const refs = getRefs();
 // refs.loadingText.classList.add('isHidden');
 // refs.errorText.classList.add('isHidden');
+
+function isReady() {
+    console.log('uoouipoipipipi');
+    refs.loadingText.classList.toggle('isHidden');
+}
+
+function selectBuilds() {
+  const selectCatsBreed = new SlimSelect({
+    select: refs.breedSelect,
+    settings: {
+      showSearch: false,
+      placeholderText: 'Choose a breed',
+    },
+  });
+}
+
 
 API.fetchBreeds(refs.loadingText, 'https://api.thecatapi.com/v1/breeds').then(
   renderBreedsList
@@ -15,31 +33,35 @@ function renderBreedsList(breedsList) {
     ({ id, name }) => `<option value="${id}">${name}</option>`
   );
   const breedsString =
-    `<option value="qwqwq">Error Positionn</option>` + breeds.join('');
-  refs.catSearch.insertAdjacentHTML('afterbegin', breedsString);
+    `<option data-placeholder="true"></option><option value="qwqwq">Error Positionn</option>` +
+    breeds.join('');
+  refs.breedSelect.insertAdjacentHTML('afterbegin', breedsString);
   refs.loadingText.classList.toggle('isHidden');
+  selectBuilds();
 }
 
-refs.catSearch.addEventListener('change', onBreesSelected);
+refs.breedSelect.addEventListener('change', onBreesSelected);
 
 function onBreesSelected(evt) {
-  const breedId = refs.catSearch.value;
+  const breedId = refs.breedSelect.value;
   API.fetchCatByBreed(refs.loadingText, breedId).then(renderSelectBreed);
 }
 
 function renderSelectBreed(breed) {
-  console.log(breed);
+//   console.log(breed);
   const { url } = breed.data[0];
   const { name, description, temperament } = breed.data[0].breeds[0];
   const catInfoString = makecatInfoString(url, name, description, temperament);
-  refs.catInfo.innerHTML = catInfoString;
-  refs.loadingText.classList.toggle('isHidden');
+    refs.catInfo.innerHTML = catInfoString;
+    
+    const currentCatImg = document.querySelector('#current-cat-img')
+    currentCatImg.addEventListener('load', isReady);
 }
 
 function makecatInfoString(url, name, description, temperament) {
   return `<ul style="list-style-type: none; display: flex;">
   <li>
-  <img src="${url}" alt="${name}" width="460px" />
+  <img id="current-cat-img" src="${url}" alt="${name}" width="460px" />
   </li>
   <li style="padding-left: 30px;">
   <h2>${name}</h2>
